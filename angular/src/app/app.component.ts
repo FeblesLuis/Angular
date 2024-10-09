7efbe9ac1c1b7c5353b7603cff69c89e455ce2dc
Wasabiii <lffebles.19@est.ucab.edu.ve>
2024-10-09 17:01:28 -0400

    Feat: Clases y estilos dinamicos

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgClass, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent 
+{
+  isDark: boolean = true;
+
+  toggleDark()
+  {
+    this.isDark = !this.isDark;
+  }
 }
