d0071bf2c4d8e03082d09930d06c7dae146c1194
Wasabiii <lffebles.19@est.ucab.edu.ve>
2024-10-10 01:11:06 -0400

    Feat:Aplicando el OnInit

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit { //El OnInit sirve para los servicios o cosas que necesitamos que se creen despues del constructor
+  constructor()
+  {
+    console.log("Metodo que se ejecuta primero")
+  }
+  
+  ngOnInit(): void 
+  {
+    console.log("Metodo que se ejecuta despues del contructor")
+  }
 }
--- angular/src/app/pages/movie-details/movie-details.component.ts
+++ angular/src/app/pages/movie-details/movie-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../../services/movie.service';
@@ -11,13 +11,17 @@ import Movie from '../../models/Movie';
   templateUrl: './movie-details.component.html',
   styleUrl: './movie-details.component.css'
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit{
 
   selectedMovie? : Movie;
 
-  constructor(private route:ActivatedRoute, private movieService: MovieService)
-  {
-    const movieName = route.snapshot.params['movieName'];
-    this.selectedMovie = movieService.getMovie(movieName);
+  constructor(
+    private route:ActivatedRoute, 
+    public movieService: MovieService)
+  {}
+
+  ngOnInit(): void {
+    const movieName = this.route.snapshot.params['movieName'];
+    this.selectedMovie = this.movieService.getMovie(movieName);
   }
 }
